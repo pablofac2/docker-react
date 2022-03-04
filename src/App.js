e0fb162efdcea21cd4e63599a924161d98916566
Pablo Caccialupi <pablofac@gmail.com>
2022-03-03 22:49:31 -0300

    firt commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit Cacha de nuevo HOLA <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
