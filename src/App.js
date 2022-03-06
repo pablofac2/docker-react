01abd4d97a75deeaf8a67d10280b0d2a6cf0c259
Pablo Caccialupi <pablofac@gmail.com>
2022-03-06 04:48:35 -0300

    jenkins test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit Cacha de nuevo HOLAAAA!! <code>src/App.js</code> and save to reload.
+          Edit Cacha de nuevo HOLAAA!! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
